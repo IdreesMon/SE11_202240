89d764e168f3d15701ac5ba901458a426d7cd1cb
IdreesMon <151092705+IdreesMon@users.noreply.github.com>
2024-05-09 19:09:31 -0400

    Done

---
+++ SE111/SE137/Week6 Software Layout/software.js
@@ -0,0 +1,21 @@
+const fileExplorer = document.querySelector(".file-explorer");
+const tabs = document.querySelectorAll(".tab");
+const activeTab = document.querySelector("#active-tab");
+activeTab.innerHTML = tabs[0].textContent;
+
+document.querySelectorAll(".tab").forEach((tab, i) => {
+    tab.addEventListener("click", () => {
+        activeTab.innerHTML = tab.textContent;
+    })
+})
+
+document.querySelector("button#toggle-button").addEventListener("click", () => {
+    console.log("button was clicked");
+    const isHidden = fileExplorer.style.display === "none";
+
+    if (isHidden) {
+        fileExplorer.style.display = "block"
+    } else {
+        fileExplorer.style.display = "none"
+    }
+})
