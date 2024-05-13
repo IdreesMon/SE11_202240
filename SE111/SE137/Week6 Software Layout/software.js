266eb6b90f16d6aa3b1540bce4b343045a6ac569
IdreesMon <151092705+IdreesMon@users.noreply.github.com>
2024-05-13 14:23:15 -0400

    Repo update

--- SE111/SE137/Week6 Software Layout/software.js
+++ SE111/SE137/Week6 Software Layout/software.js
@@ -2,9 +2,12 @@ const fileExplorer = document.querySelector(".file-explorer");
 const tabs = document.querySelectorAll(".tab");
 const activeTab = document.querySelector("#active-tab");
 activeTab.innerHTML = tabs[0].textContent;
+tabs[0].classList.add("selected");
 
 document.querySelectorAll(".tab").forEach((tab) => {
     tab.addEventListener("click", () => {
+        document.querySelector(".selected").classList.remove("selected");
+        tab.classList.add("selected");
         activeTab.innerHTML = tab.textContent;
     })
 })
